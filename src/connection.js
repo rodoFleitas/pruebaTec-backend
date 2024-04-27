a07bc08b6d1b70733bdfa0f7e0972d7f65b3d2c1
rodoFleitas <rfleitasarce@gmail.com>
2024-04-27 14:42:47 -0400

    connection to db

---
+++ src/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const { MONGO_URI } = process.env;
+
+exports.connectToDB = () => {
+  console.log(`--> Connecting to DB...`);
+  return mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
